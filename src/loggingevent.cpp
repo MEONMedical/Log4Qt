f75faf3f8c967e14225b7217c68620851388094f
bachera2 <andreas.bacher.ab1@roche.com>
2010-07-05 14:49:30 +0200

    If object name is not defined for thread use thread ID instead if object name for thread identification

--- src/loggingevent.cpp
+++ src/loggingevent.cpp
@@ -5,21 +5,21 @@
  * created:     September 2007
  * author:      Martin Heinrich
  *
- * 
+ *
  * Copyright 2007 Martin Heinrich
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  *****************************************************************************/
 
 
@@ -47,28 +47,28 @@
 
 namespace Log4Qt
 {
-	
-	
+
+
 	/**************************************************************************
 	 * Declarations
 	 **************************************************************************/
-	
-	    
+
+
 	LOG4QT_GLOBAL_STATIC(QMutex, sequence_guard)
-	
-	
-	
+
+
+
 	/**************************************************************************
 	 * C helper functions
 	 **************************************************************************/
-	
-	
-	
+
+
+
 	/**************************************************************************
 	 * Class implementation: LoggingEvent
 	 **************************************************************************/
-	
-	
+
+
 	LoggingEvent::LoggingEvent() :
 		mLevel(Level::NULL_INT),
 	    mpLogger(0),
@@ -81,9 +81,9 @@ namespace Log4Qt
 	{
 		setThreadNameToCurrent();
 	}
-	
-	
-	LoggingEvent::LoggingEvent(const Logger *pLogger, 
+
+
+	LoggingEvent::LoggingEvent(const Logger *pLogger,
 	                           Level level,
 	                           const QString &rMessage) :
 	    mLevel(level),
@@ -97,8 +97,8 @@ namespace Log4Qt
 	{
 		setThreadNameToCurrent();
 	}
-	
-	
+
+
 	LoggingEvent::LoggingEvent(const Logger *pLogger,
 	                           Level level,
 	                           const QString &rMessage,
@@ -114,10 +114,10 @@ namespace Log4Qt
 	{
 		setThreadNameToCurrent();
 	}
-	
-	
-	LoggingEvent::LoggingEvent(const Logger *pLogger, 
-	                           Level level, 
+
+
+	LoggingEvent::LoggingEvent(const Logger *pLogger,
+	                           Level level,
 	                           const QString &rMessage,
 	                           const QString &rNdc,
 	                           const QHash<QString, QString> &rProperties,
@@ -133,70 +133,75 @@ namespace Log4Qt
 	    mTimeStamp(timeStamp)
 	{
 	}
-	
-	
+
+
 	QString LoggingEvent::loggerName() const
-	{	
+	{
 		if (mpLogger)
 			return mpLogger->name();
 		else
 			return QString();
 	}
-	
-	
+
+
 	QString LoggingEvent::toString() const
-	{   
+	{
 	    return level().toString() + QLatin1Char(':') + message();
 	}
-	
-	
+
+
 	qint64 LoggingEvent::sequenceCount()
 	{
 	    QMutexLocker locker(sequence_guard());
-	    
+
 	    return msSequenceCount;
-	} 
-	
-	
+	}
+
+
 	qint64 LoggingEvent::startTime()
 	{
 		return InitialisationHelper::startTime();
 	}
-	
-	
+
+
 	void LoggingEvent::setThreadNameToCurrent()
 	{
 		if (QThread::currentThread())
+		{
 			mThreadName = QThread::currentThread()->objectName();
+			// if object name is not set use thread id for thead identification
+			if (mThreadName.isEmpty())
+				mThreadName = QString("0x%0").arg(QThread::currentThreadId(),4,16);
+		}
 	}
-	
-	
+
+
 	qint64 LoggingEvent::nextSequenceNumber()
-	{   
+	{
 	    QMutexLocker locker(sequence_guard());
-	    
-	    return ++msSequenceCount;    
+
+	    return ++msSequenceCount;
 	}
-	
-	
+
+
 	qint64 LoggingEvent::msSequenceCount = 0;
-	
-	
-	
+
+
+
 	/**************************************************************************
 	 * Implementation: Operators, Helper
 	 **************************************************************************/
-	
-	
+
+
 #ifndef QT_NO_DATASTREAM
     QDataStream &operator<<(QDataStream &rStream, const LoggingEvent &rLoggingEvent)
     {
         QBuffer buffer;
         buffer.open(QIODevice::WriteOnly);
         QDataStream stream(&buffer);
-        
+
         // version
-        quint16 version = 0; 
+        quint16 version = 0;
         stream << version;
         // version 0 data
         stream << rLoggingEvent.mLevel
@@ -207,13 +212,13 @@ namespace Log4Qt
                << rLoggingEvent.mSequenceNumber
                << rLoggingEvent.mThreadName
                << rLoggingEvent.mTimeStamp;
-        
+
         buffer.close();
         rStream << buffer.buffer();
-        return rStream; 
+        return rStream;
     }
-    
-    
+
+
     QDataStream &operator>>(QDataStream &rStream, LoggingEvent &rLoggingEvent)
     {
         QByteArray array;
@@ -221,9 +226,9 @@ namespace Log4Qt
         QBuffer buffer(&array);
         buffer.open(QIODevice::ReadOnly);
         QDataStream stream(&buffer);
-        
+
         // version
-        quint16 version; 
+        quint16 version;
         stream >> version;
         // Version 0 data
         QString logger;
@@ -239,22 +244,22 @@ namespace Log4Qt
             rLoggingEvent.mpLogger = 0;
         else
             rLoggingEvent.mpLogger = Logger::logger(logger);
-        
+
         buffer.close();
-        return rStream; 
+        return rStream;
     }
 #endif // QT_NO_DATASTREAM
 
-    
+
 #ifndef QT_NO_DEBUG_STREAM
-	QDebug operator<<(QDebug debug, 
+	QDebug operator<<(QDebug debug,
 	                  const LoggingEvent &rLoggingEvent)
 	{
 	    QString logger;
 	    if (rLoggingEvent.logger() != 0)
 	        logger = rLoggingEvent.logger()->name();
-	    
-	    debug.nospace() << "LoggingEvent(" 
+
+	    debug.nospace() << "LoggingEvent("
 	        << "level:" << rLoggingEvent.level().toString() << " "
 	        << "logger:" << logger << " "
 	        << "message:" << rLoggingEvent.message() << " "
@@ -267,6 +272,6 @@ namespace Log4Qt
 	    return debug.space();
 	}
 #endif // QT_NO_DEBUG_STREAM
-	
-	
+
+
 } // namespace Log4Qt
