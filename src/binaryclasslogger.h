1032ecc3e93e716ac54a38358a5fa49ceae4b9fb
Andreas Bacher <andreas.bacher@meon-medical.com>
2016-02-26 12:18:26 +0100

    Reverted change in class loggers - revert static initialization

---
+++ src/binaryclasslogger.h
@@ -0,0 +1,27 @@
+#ifndef LOG4QT_BINARYCLASSLOGGER_H
+#define LOG4QT_BINARYCLASSLOGGER_H
+
+#include "../log4qtshared.h"
+
+#include <QtCore/QObject>
+#include <QtCore/QAtomicPointer>
+
+namespace Log4Qt
+{
+
+class Logger;
+class BinaryLogger;
+
+class LOG4QT_EXPORT BinaryClassLogger
+{
+public:
+    BinaryClassLogger();
+    BinaryLogger *logger(const QObject *pObject);
+
+private:
+    mutable QAtomicPointer<BinaryLogger> mpLogger;
+};
+
+} // namespace Log4Qt
+
+#endif // LOG4QT_BINARYCLASSLOGGER_H
